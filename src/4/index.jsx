dc0578f28e8923eed0ea2dfc2cef91d86cdb0e3b
Sardor <sardoribodov07@gmail.com>
2023-07-06 15:44:53 +0500

    State | 2

---
+++ src/4/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { database } from "./database";
+
+class Lesson4 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      surName: "",
+      students: database,
+    };
+  }
+  render() {
+    const { firstName, surName, students } = this.state;
+    // const onChangeName = ({ target: { value } }) => {
+    //   this.setState({ firstName: value });
+    // };
+    // const onChangeSurName = ({ target: { value } }) => {
+    //   this.setState({ surName: value });
+    // };
+    const onChangeInfo = ({ target }) => {
+      this.setState({ [target.name]: target.value });
+    };
+    const onSearch = ({ target: { value } }) => {
+      let filtered = database.filter((s) =>
+        s.fullName.toLowerCase().includes(value.toLowerCase())
+      );
+      console.log(filtered);
+      this.setState({ students: filtered });
+    };
+    return (
+      <div>
+        <h1>State | 2</h1>
+        <h1>Ism: {firstName}</h1>
+        <h1>Familiya: {surName}</h1>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Ism kiriting"
+          onChange={onChangeInfo}
+        />
+        <br />
+        <input
+          type="text"
+          name="surName"
+          placeholder="Familiya kiriting"
+          onChange={onChangeInfo}
+        />
+        <hr />
+        <input type="text" placeholder="Type to search" onChange={onSearch} />
+        {students.map((s) => (
+          <h1>
+            {s.id}. {s.fullName}
+          </h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Lesson4;
