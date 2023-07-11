f7163e4cc1c5d6c9d8d48da6a4e3ce8a1595308d
Sardor <sardoribodov07@gmail.com>
2023-07-11 15:26:41 +0500

    CRUD in React

---
+++ src/crud/database.js
@@ -0,0 +1,8 @@
+export const database = [
+  { id: 1, fullName: "Rasulov Jamshidbek", age: 21 },
+  { id: 2, fullName: "Akbar Ochilov", age: 30 },
+  { id: 3, fullName: "MuhammadYahyo Latifov", age: 22 },
+  { id: 4, fullName: "Turgunaliev Abdulahad", age: 18 },
+  { id: 5, fullName: "Bahriddinov Zuhriddin", age: 24 },
+  { id: 6, fullName: "Xabibullayev Dilmurod", age: 20 },
+];
---
+++ src/crud/index.jsx
@@ -0,0 +1,185 @@
+import React from "react";
+import { database } from "./database";
+
+let base = database;
+
+class CRUD extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: base,
+      sortType: "id",
+      newFullname: "",
+      newAge: "",
+      selectedStudent: null,
+      editedFullname: "",
+      editedAge: "",
+    };
+  }
+  render() {
+    const {
+      students,
+      sortType,
+      newFullname,
+      newAge,
+      selectedStudent,
+      editedFullname,
+      editedAge,
+    } = this.state;
+    //! Delete
+    const deleteStudent = (id) => {
+      base = base.filter((s) => s.id !== id);
+      this.setState({ students: base });
+    };
+    //! Read
+    //? Find
+    const findStudent = (searchedStudent) => {
+      let res = base.filter((s) =>
+        s.fullName.toLowerCase().includes(searchedStudent.toLowerCase())
+      );
+      this.setState({ students: res });
+    };
+    //? Sort
+    const sortStudent = () => {
+      let res = base.sort((a, b) => {
+        if (sortType === "id") return a.id - b.id;
+        else if (sortType === "fullName")
+          return a.fullName.localeCompare(b.fullName);
+        else return a.age - b.age;
+      });
+      this.setState({ students: res });
+    };
+    //! Create
+    function* generateId() {
+      let length = base.length;
+      while (true) {
+        yield ++length;
+      }
+    }
+    const addStudent = () => {
+      base = [
+        ...base,
+        { id: generateId().next().value, fullName: newFullname, age: newAge },
+      ];
+      this.setState({ students: base });
+    };
+    //! Edit
+    const save = (id) => {
+      base = base.map((s) => {
+        return s.id === id
+          ? {
+              ...s,
+              fullName: editedFullname,
+              age: editedAge,
+            }
+          : s;
+      });
+      this.setState({ students: base, selectedStudent: null });
+    };
+
+    return (
+      <div>
+        <h1>CRUD</h1>
+        <div className="find">
+          <input
+            type="text"
+            placeholder="Type to search"
+            onChange={({ target: { value } }) => findStudent(value)}
+          />
+        </div>
+        <div className="sort">
+          <select
+            onChange={({ target: { value } }) =>
+              this.setState({ sortType: value })
+            }
+          >
+            <option value="id">Id</option>
+            <option value="fullName">Fullname</option>
+            <option value="age">Age</option>
+          </select>
+          <button onClick={sortStudent}>Sort</button>
+        </div>
+        <div className="add">
+          <input
+            type="text"
+            placeholder="Fullname"
+            value={newFullname}
+            onChange={({ target: { value } }) =>
+              this.setState({ newFullname: value })
+            }
+          />
+          <input
+            type="number"
+            placeholder="Age"
+            value={newAge}
+            onChange={({ target: { value } }) =>
+              this.setState({ newAge: value })
+            }
+          />
+          <button
+            onClick={() => {
+              addStudent();
+              this.setState({ newFullname: "", newAge: "" });
+            }}
+          >
+            Add
+          </button>
+        </div>
+
+        {students.map(({ id, fullName, age }) => (
+          <h1>
+            {id}.
+            {id === selectedStudent ? (
+              <input
+                type="text"
+                defaultValue={fullName}
+                onChange={({ target: { value } }) =>
+                  this.setState({ editedFullname: value })
+                }
+              />
+            ) : (
+              fullName
+            )}
+            {id === selectedStudent ? (
+              <input
+                type="number"
+                defaultValue={age}
+                onChange={({ target: { value } }) =>
+                  this.setState({ editedAge: value })
+                }
+              />
+            ) : (
+              age
+            )}{" "}
+            yosh
+            <button onClick={() => deleteStudent(id)}>Delete</button>
+            {selectedStudent === id ? (
+              <span>
+                <button onClick={() => save(id)}>Save</button>
+                <button
+                  onClick={() => this.setState({ selectedStudent: null })}
+                >
+                  Cancel
+                </button>
+              </span>
+            ) : (
+              <button
+                onClick={() =>
+                  this.setState({
+                    selectedStudent: id,
+                    editedFullname: fullName,
+                    editedAge: age,
+                  })
+                }
+              >
+                Edit
+              </button>
+            )}
+          </h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CRUD;
